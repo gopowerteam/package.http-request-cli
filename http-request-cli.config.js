a3c2f4e89d6e7b3e0acb791ed313c51861798609
zhuchentong <2037630@163.com>
2022-01-17 12:56:12 +0800

    锁定版本依赖

--- http-request-cli.config.js
+++ http-request-cli.config.js
@@ -1,60 +1,41 @@
-module.exports = [
-  {
-    name: "default",
-    gateway: "http://gateway.local.xbt-dev.top",
-    swagger: "v2/api-docs",
-    model: true,
-    modelDir: {
-      alias: "@/http/model", // 控制器目录名别
-      path: "./data/model", // 控制器目录路径
-    },
-    controllerDir: {
-      alias: "@/http/controller", // 控制器目录名别
-      path: "./data/controller", // 控制器目录路径
-    },
-    serviceDir: {
-      alias: "@/http/services", // 服务目录名别
-      path: "./data/services", // 服务目录名别
-    },
-    services: {
-      "communication-service": "xbt-platform-communication-service",
-      "wechat-service": "xbt-platform-wxcp-service",
-      "eky-service": "xbt-platform-data-service",
-      "customer-service": "xbt-platform-customer-service",
-    },
-    alias: [
-      {
-        service: "wechat-service",
-        from: "Department",
-        to: "WechatDepartment",
-      },
-      {
-        service: "customer-service",
-        from: "ConsultationRecord",
-        to: "WxConsultationRecord",
-      },
-      {
-        service: "customer-service",
-        from: "Appointment",
-        to: "WxAppointment",
-      },
-    ],
+module.exports = {
+  name: "default",
+  gateway: "http://gateway.local.xbt-dev.top",
+  swagger: "v2/api-docs",
+  model: true,
+  modelDir: {
+    alias: "@/http/model", // 控制器目录名别
+    path: "./data/model", // 控制器目录路径
   },
-  {
-    name: "authorization",
-    gateway: "https://authorization.local.xbt-dev.top",
-    swagger: "api-docs-json",
-    controllerDir: {
-      alias: "@/http/controller",
-      path: "./data/controller",
-    },
-    serviceDir: {
-      alias: "@/http/services",
-      path: "./data/services",
-    },
-    alias: {
-      from: "App",
-      to: "Acc",
-    },
+  controllerDir: {
+    alias: "@/http/controller", // 控制器目录名别
+    path: "./data/controller", // 控制器目录路径
   },
-];
+  serviceDir: {
+    alias: "@/http/services", // 服务目录名别
+    path: "./data/services", // 服务目录名别
+  },
+  services: {
+    "communication-service": "xbt-platform-communication-service",
+    "wechat-service": "xbt-platform-wxcp-service",
+    "eky-service": "xbt-platform-data-service",
+    "customer-service": "xbt-platform-customer-service",
+  },
+  alias: [
+    {
+      service: "wechat-service",
+      from: "Department",
+      to: "WechatDepartment",
+    },
+    {
+      service: "customer-service",
+      from: "ConsultationRecord",
+      to: "WxConsultationRecord",
+    },
+    {
+      service: "customer-service",
+      from: "Appointment",
+      to: "WxAppointment",
+    },
+  ],
+};
